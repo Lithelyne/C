cb3ab0cfd2ed99dee9ccc125a1203a19bfd31e9f
Lithelyne <126444415+Lithelyne@users.noreply.github.com>
2023-06-08 23:50:32 -0700

    Finished assign

--- DojoSurvey/Controllers/HomeController.cs
+++ DojoSurvey/Controllers/HomeController.cs
@@ -13,11 +13,27 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    [HttpGet("")]
     public IActionResult Index()
     {
         return View();
     }
 
+    [HttpPost("showUser")]
+    public IActionResult AddUser(string Name, string Location, string Language, string Comments)
+    {
+        ViewBag.Name = Name;
+        ViewBag.Location = Location;
+        ViewBag.Language = Language;
+        ViewBag.Comments = Comments;
+
+
+        return View("showUser");
+    }
+
+
+
+    [HttpGet("privacy")]
     public IActionResult Privacy()
     {
         return View();
